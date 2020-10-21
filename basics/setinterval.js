8dfd1714d5c6cbca743fe9aa52fdd11fc5775ab5
hemendrta <hemendra.singh.c@outlook.com>
2020-10-21 11:46:43 -0700

    Updated content on October 21, 2020

--- basics/setinterval.js
+++ basics/setinterval.js
@@ -1,5 +1,17 @@
-setInterval(() => {
+// Set time interval which will execute for every one second
+
+const myvar=setInterval(() => {
 
     var date=new Date();
     console.log(date.toLocaleTimeString());
+
 }, 1000);
+
+// set time out function which will be called at 3 second 
+
+setTimeout(() => {
+
+// Clear timeout which will stop the execution of timeinterval
+
+    clearTimeout(myvar);
+}, 3000);
