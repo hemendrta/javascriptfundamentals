2590e7280816f5e95cf18a34a1309bf512169271
hemendrta <hemendra.singh.c@outlook.com>
2020-11-24 17:00:15 -0800

    Practice Questions updated

--- basics/practicequestions.js
+++ basics/practicequestions.js
@@ -196,10 +196,13 @@ const obj4 = { a: 4, b: 5, c: 6, d: 10 };
 // false
 // true
 // true
+// true
+// false
 
 const matchObject = (objA, objB) => {
   return Object.keys(objA).every((key) => objB[key]);
 };
+
 console.log("--------------------");
 console.log(matchObject(obj1, obj2));
 console.log(matchObject(obj1, obj4));
@@ -282,3 +285,117 @@ console.log(checkVoewlsCount("singh chouhan"));
 console.log(checkVoewlsCount("hemendra"));
 
 console.log("-----------------------------------");
+
+// Extract unique characters from a string.
+
+const getUniqueCharactersFromString = (name) =>
+  [...new Set(name.split(""))].join();
+
+console.log(getUniqueCharactersFromString("hemendra singh chouhan"));
+console.log(getUniqueCharactersFromString("nisha singh chouhan"));
+console.log(getUniqueCharactersFromString("subhash singh chouhan"));
+console.log(getUniqueCharactersFromString("aditi singh chouhan"));
+
+console.log("-----------------------------------");
+
+// Question:
+
+const person = [
+  {
+    firstName: "Hemendra",
+    lastName: "Chouhan",
+    DOB: "11/28/1989",
+    department: "Software Development",
+    salary: "50000",
+  },
+  {
+    firstName: "Subhash",
+    lastName: "Chouhan",
+    DOB: "11/28/1958",
+    department: "Administration",
+    salary: "40000",
+  },
+  {
+    firstName: "Aditi",
+    lastName: "Chouhan",
+    DOB: "10/04/1989",
+    department: "Human Resource",
+    salary: "33000",
+  },
+  {
+    firstName: "Nisha",
+    lastName: "Chouhan",
+    DOB: "08/25/1956",
+    department: "Education",
+    salary: "50012",
+  },
+  {
+    firstName: "Sunny",
+    lastName: "Chouhan",
+    DOB: "11/19/1989",
+    department: "Information Technology",
+    salary: "45000",
+  },
+  {
+    firstName: "Rahul",
+    lastName: "Shrivastava",
+    DOB: "09/23/1989",
+    department: "Recruitment",
+    salary: "80000",
+  },
+  {
+    firstName: "Harsh",
+    lastName: "Gautam",
+    DOB: "01/09/1986",
+    department: "Recruitment",
+    salary: "52000",
+  },
+  {
+    firstName: "Ramesh",
+    lastName: "Chouhan",
+    DOB: "09/23/1982",
+    department: "Training Development",
+    salary: "60000",
+  },
+];
+
+// Average Salary for the persons
+
+const getAverageSalary = (person1) =>
+  person1.reduce(
+    (accumulator, person1) => accumulator + parseInt(person1.salary),
+    0
+  ) / person1.length;
+
+console.log(getAverageSalary(person));
+
+console.log("---------------------------------------");
+
+// People who are above 32
+
+const getPersonAbove32 = (person) =>
+  person.filter(
+    (per) => new Date().getFullYear() - new Date(per.DOB).getFullYear() > 32
+  );
+
+console.log(getPersonAbove32(person));
+
+console.log("---------------------------------------");
+
+// Get Full Name
+
+const getFullName = (person) =>
+  person.map((p) => `${p.firstName} ${p.lastName}`);
+
+console.log(getFullName(person));
+
+console.log("---------------------------------------");
+
+// Sorted on the basis of Age (Youngest to Oldest)
+
+const sortingAge = (person) =>
+  person.sort(
+    (person1, person2) => new Date(person2.DOB) - new Date(person1.DOB)
+  );
+
+console.log(sortingAge(person));
